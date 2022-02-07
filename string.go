eb9d6580743c25cdf306f14893891ab7a3e35b96
taufik17 <taufikagungsantoso17@gmail.com>
2022-02-07 07:52:09 +0700

    bool, num, string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Taufik")
+	fmt.Println("Taufik Agung")
+	fmt.Println("Taufik Agung Santoso")
+
+	fmt.Println(len("Taufik"))
+	fmt.Println("Taufik Agung"[0])
+	fmt.Println("Taufik Agung Santoso"[1])
+}
